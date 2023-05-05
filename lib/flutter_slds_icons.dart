ee5408c4ae05e9370208af4f0c0a7f8e855bfca8
MJ12358 <wtpltlf.dev@gmail.com>
2023-05-05 02:06:48 -0500

    use standard file name convention

--- lib/flutter_slds_icons.dart
+++ lib/flutter_slds_icons.dart
@@ -2,8 +2,8 @@ library flutter_slds_icons;
 
 import 'package:flutter/widgets.dart';
 
-part 'src/SLDSActionIcons.dart';
-part 'src/SLDSCustomIcons.dart';
-part 'src/SLDSDoctypeIcons.dart';
-part 'src/SLDSStandardIcons.dart';
-part 'src/SLDSUtilityIcons.dart';
+part 'src/action.dart';
+part 'src/custom.dart';
+part 'src/doctype.dart';
+part 'src/standard.dart';
+part 'src/utility.dart';
