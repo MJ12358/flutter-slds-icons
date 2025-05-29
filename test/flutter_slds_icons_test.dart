8a55512b0030d15993bcb7af43edc72a47d98279
MJ12358 <wtpltlf.dev@gmail.com>
2025-05-29 15:21:18 -0500

    v2.0.0

--- test/flutter_slds_icons_test.dart
+++ test/flutter_slds_icons_test.dart
@@ -5,10 +5,11 @@ import 'package:flutter_test/flutter_test.dart';
 import '../example/lib/map.dart';
 
 void main() {
+  // This test is to ensure that the icons are correctly defined.
+  // It checks if all icons in the map are valid IconData instances.
   test('verify icons', () {
-    // This test is to ensure that the icons are correctly defined.
-    // It checks if all icons in the map are valid IconData instances.
     expect(kIcons.isNotEmpty, true);
+
     for (final MapEntry<String, IconData> entry in kIcons.entries) {
       expect(entry.value, isA<IconData>());
       expect(entry.key.isNotEmpty, true);
